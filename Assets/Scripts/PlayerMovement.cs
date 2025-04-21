3f2f751ed5b084389ed5858eb79be91f94e36d40
PairojKurakanok <pairoj.kura@bumail.net>
2025-04-21 16:27:12 +0700

    ใส่ Asset

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
